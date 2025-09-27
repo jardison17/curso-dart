39a03e38c30e1172633dc790f1714b8418df6aa8
jardison17 <jallima1997@gmail.com>
2025-09-27 14:36:29 -0400

    quinto commit

--- fundamentos/operadores_1.dart
+++ fundamentos/operadores_1.dart
@@ -1,8 +1,25 @@
-// ignore_for_file: dead_code
+
+
+// "&&" -> AND (E)
+// "||" -> OR (OU
+// "^"  -> XOR (OU Exclusivo)
+// "!"  -> NOT (NÃO)
+
 
 void main() {
-/*
-  //  Operadores Aritiméticos 
+
+  /*Operadores Aritiméticos*/
+
+ 
+ // "int" ->  variavel do tipo inteiro
+ //  "d"  -> nome da variável
+ //  "="  -> operador de atribuição
+ //  "25" -> valor atribuído a variável
+   int d  = 25;
+ // "int" -> inteiro
+ // "double" -> número com ponto flutuante
+
+
   int a = 10;
   int b = 15;
   int c = 20;
@@ -14,17 +31,31 @@ void main() {
   print(a * b);               // 10 * 15 = 150
   print(c / a);               // 20 / 10 = 2.0
   print(a + b * c);           // 10 + 15 * 20 = 310  
-*/    
+  print(d);                  // 25
+    
 
   //  Operadores Lógicos
   bool ehFragil = true;
   bool ehCaro = false;
 
+
   print(ehFragil && ehCaro);   //  AND -> E
-  print(ehFragil || ehCaro);   // OR -> OU
+  // ehFragil && ehCaro  ->  são ambos verdadeiros?
+
+
+
+
+ print(ehFragil | ehCaro);    // OR -> OU
+  // ehFragil || ehCaro  ->  é um ou outro verdadeiro?
+
+
   print(ehFragil ^ ehCaro);    // XOR -> OU Exclusivo
+  // ehFragil ^ ehCaro  ->  um é verdadeiro, mas não ambos
+
   print(!ehFragil);            // NOT (NÃO) -> Unário/Prefix
-  print(!ehFragil);            // NOT (NÃO) -> Unário/Prefix
+  // !ehFragil  ->  inverte o valor lógico
+
+
 
 
 
---
+++ função/função_1.dart
@@ -0,0 +1,18 @@
+main () {
+
+ int a = 2;
+ int b = 3;
+ print(a + b);          // resultado 2 + 3 = 5
+
+ int c = 4;
+ int d = 5;
+ print(c + d);          // resultado 4 + 5 = 9
+
+ somaComPrint();       // resultado executei!!!
+
+
+}
+
+somaComPrint() {
+  print('executei!!!');
+}
